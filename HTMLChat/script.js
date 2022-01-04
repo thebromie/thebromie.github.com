b8df5fb6cbb9d7c29b139646fcd59ca24afc61f7
Vinay <thebromie153@gmail.com>
2022-01-04 16:55:07 -0500

    Hidden Keys

--- HTMLChat/script.js
+++ HTMLChat/script.js
@@ -1,6 +1,7 @@
 function PopUp(){
     let ChannelID = "UC7C948AM_7cNIORd22Rr_SQ";
     var xhr = new XMLHttpRequest();
+    var key = config.SECRET_API_KEY;
     xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
     let result = JSON.parse(xhr.responseText);
@@ -13,7 +14,7 @@ function PopUp(){
     }
     }
     };
-    xhr.open("GET", `https://www.googleapis.com/youtube/v3/search?part=id&channelId=${ChannelID}&type=video&eventType=live&key=AIzaSyB_6Y_WY4v5omvjLykCGow3sBsitd5eSJY`, true);
+    xhr.open("GET", `https://www.googleapis.com/youtube/v3/search?part=id&channelId=${ChannelID}&type=video&eventType=live&key=${key}`, true);
     xhr.send();
 }
 
