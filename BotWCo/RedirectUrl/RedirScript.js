56ae2d25cedc635e4669e448ba565d64c50e30bf
Vinay <thebromie153@gmail.com>
2022-01-15 16:20:35 -0500

    should be working now

--- BotWCo/RedirectUrl/RedirScript.js
+++ BotWCo/RedirectUrl/RedirScript.js
@@ -2,7 +2,7 @@ function LiveFinder(){
     //Big Thanks to Komali for the API Search Code
     //https://github.com/PrinceKomali
     
-    let ChannelID = "UC7C948AM_7cNIORd22Rr_SQ";
+    let ChannelID = "UCSJ4gkVC6NrvII8umztf0Ow";
     
     let key = "AIzaSyD3GqHtog__j_ef47-sF00-g-ZSvrkIMR4";
     var xhr = new XMLHttpRequest();
